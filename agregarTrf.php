4141a4e4ef7c220ba20d76f0f7914fe59db48c91
machucagelvez <machucagelvez@gmail.com>
2020-11-19 14:02:14 -0500

    Update agregarTrf.php

--- agregarTrf.php
+++ agregarTrf.php
@@ -17,12 +17,21 @@ else
 {
 	$saldo_destino = mysqli_fetch_array(mysqli_query($cnx, "select saldo from cuenta where Nrocuenta = '$NroCtaDest'"));
 	$saldo_origen = mysqli_fetch_array(mysqli_query($cnx, "select saldo from cuenta where Nrocuenta = '$NroCtaOrigen'"));
-	$saldo_destino[0] = $saldo_destino[0] + $Valor;
-	$saldo_origen[0] = $saldo_origen[0] - $Valor;
-	mysqli_query($cnx,"INSERT INTO transaccion (NroCtaOrigen,NroCtaDest,Hora,Fecha,Valor) VALUES ('$NroCtaOrigen','$NroCtaDest','$Hora','$Fecha','$Valor')");
-	mysqli_query($cnx, "UPDATE cuenta SET saldo = '$saldo_destino[0]' WHERE Nrocuenta = '$NroCtaDest'");
-	mysqli_query($cnx, "UPDATE cuenta SET saldo = '$saldo_origen[0]' WHERE Nrocuenta = '$NroCtaOrigen'");
-	$rta = "1";
+	$resto = $saldo_origen[0]-10000;
+	if ($resto>=$Valor)
+	{
+		$saldo_destino[0] = $saldo_destino[0] + $Valor;
+		$saldo_origen[0] = $saldo_origen[0] - $Valor;
+		mysqli_query($cnx,"INSERT INTO transaccion (NroCtaOrigen,NroCtaDest,Hora,Fecha,Valor) VALUES ('$NroCtaOrigen','$NroCtaDest','$Hora','$Fecha','$Valor')");
+		mysqli_query($cnx, "UPDATE cuenta SET saldo = '$saldo_destino[0]' WHERE Nrocuenta = '$NroCtaDest'");
+		mysqli_query($cnx, "UPDATE cuenta SET saldo = '$saldo_origen[0]' WHERE Nrocuenta = '$NroCtaOrigen'");
+		$rta = "1";
+	}
+	else
+	{
+		$rta="2";
+	}
+	
 	
 }
 echo $rta;
